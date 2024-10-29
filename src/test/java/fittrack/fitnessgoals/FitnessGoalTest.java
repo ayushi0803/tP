fcbe38d34e5e590b6334ad43b9213280a5729d18
Ayushi <e0905342@u.nus.edu>
2024-10-29 23:54:10 +0800

    checkstyle fixing fitnessgoaltest

--- src/test/java/fittrack/fitnessgoals/FitnessGoalTest.java
+++ src/test/java/fittrack/fitnessgoals/FitnessGoalTest.java
@@ -6,7 +6,7 @@ import fittrack.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayOutputStream; 
 import java.io.PrintStream;
 import java.time.LocalDateTime;
 

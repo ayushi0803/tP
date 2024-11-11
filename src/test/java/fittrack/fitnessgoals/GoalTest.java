fec4f9838b09c22d04573dde20b3371b8af33ffc
Ayushi <e0905342@u.nus.edu>
2024-11-11 22:31:53 +0800

    imports - adding necessary ones and removing redundant ones

--- src/test/java/fittrack/fitnessgoals/GoalTest.java
+++ src/test/java/fittrack/fitnessgoals/GoalTest.java
@@ -2,9 +2,11 @@ package fittrack.fitnessgoals;
 
 import fittrack.fitnessgoal.Goal;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 class GoalTest {
 
